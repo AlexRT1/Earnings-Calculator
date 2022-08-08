93047f511c676a37f50b41b0ad0e415c23edc4e3
AlexRT1 <110362663+AlexRT1@users.noreply.github.com>
2022-08-08 08:30:52 +0000

    completed challenge

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+print("hello user")
+employee_name = input("what is the employee name? ") 
+# could have added in the . title().strip here 
+employee_wage = input("what is the employee hourly rate? ")
+employee_hours_worked = input("how many hours did the employee work? ")
+
+perfect_example = employee_name.title().strip().lower()
+
+print(perfect_example)
+
+wage = float(employee_wage)
+hours = float(employee_hours_worked)
+
+print(f"{perfect_example} made ${wage * hours} this week")
+
+# ${earnings:.2f} how to do rounding 
